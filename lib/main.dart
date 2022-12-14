6caac4462bb7c6a9bec4b76d8cbf397542628e8f
AaronW <zmtzwb@gmail.com>
2022-12-14 21:45:04 +0800

    init project

---
+++ lib/main.dart
@@ -0,0 +1,220 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart' hide MenuItem;
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:tray_manager/tray_manager.dart';
+
+enum TaskType {video, music, document, unknown}
+
+class ATask {
+  ATask(this.uri, this.title) {
+    type = TaskType.unknown;
+    createTime = DateTime.now();
+    progress = Random().nextInt(100) / 100;
+  }
+
+  // 任务地址
+  final String uri;
+  // 任务类型
+  late TaskType type;
+  // 任务标题
+  late String title;
+  // 任务创建时间
+  late DateTime createTime;
+
+  late Duration leftDuration;
+
+  late DateTime finishTime;
+
+  late double progress;
+}
+
+class ATaskListView extends Container {
+  List<ATask> tasks = [];
+  ATaskListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var provider = Provider.of<ATaskProvider>(context, listen: true);
+    tasks = provider.tasks;
+    return Expanded(
+        child: ListView.builder(
+          itemCount: provider.tasks.length,
+          itemBuilder: taskBuilder
+        )
+    );
+  }
+
+  Widget taskBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Text(tasks[index].title),
+          LinearProgressIndicator(value: tasks[index].progress,)
+        ],
+      )
+    );
+  }
+}
+
+class ATaskProvider with ChangeNotifier, DiagnosticableTreeMixin {
+  List<ATask> tasks = [
+    ATask("uri", "test1"),
+    ATask("uri", "test1"),
+    ATask("uri", "test1"),
+    ATask("uri", "test1"),
+  ];
+
+  int get count => tasks.length;
+
+
+  int addTask(ATask task) {
+    tasks.add(task);
+    notifyListeners();
+    return tasks.length;
+  }
+
+  void updateTask(int index, ATask task){
+    tasks[index] = task;
+    notifyListeners();
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(IntProperty("task count", tasks.length));
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ATaskProvider())
+      ],
+      child: const MyApp()
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget with TrayListener {
+
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          OutlinedButton(
+            child: const Text("progress"),
+            onPressed: () {
+              showDialog(context: context, builder: (ctx) {
+                return AlertDialog(
+                  title: const Text("alert"),
+                  content: const TextField(),
+                  actions: [
+                    OutlinedButton(onPressed: () {
+                      debugPrint("ok");
+                    }, child: const Text("ok"))
+                  ]
+                );
+              });
+              
+              var taskProvider = context.read<ATaskProvider>();
+              taskProvider.addTask(ATask("uri", "add test"));
+
+              var timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+                var t = taskProvider.tasks[0];
+                t.progress += 0.001;
+                taskProvider.updateTask(0, t);
+                if (t.progress >= 1) {
+                  timer.cancel();
+                }
+              });
+            },
+          ),
+          MaterialButton(
+            child: const Text("http"),
+            onPressed: () async {
+              windowManager.hide();
+
+              final TrayManager trayManager = TrayManager.instance;
+              String tryIconPath = 'assets/images/avatar.ico';
+              trayManager.setIcon(tryIconPath);
+
+              Menu menu = Menu(
+                items: [
+                  MenuItem(
+                    key: "show",
+                    label: "show"
+                  ),
+                  MenuItem.separator(),
+                  MenuItem(
+                    key: 'quit',
+                    label: '退出'
+                  )
+                ]
+              );
+              await trayManager.setToolTip("hello");
+              await trayManager.setContextMenu(menu);
+              trayManager.addListener(this);
+
+              Future<http.Response> resp = http.get(Uri.parse("https://jsonplaceholder.typicode.com/albums/1"));
+              resp.then((value) => {
+                if (value.statusCode == 200) {
+                  debugPrint("ok")
+                }
+              });
+          }),
+          ATaskListView()
+        ],
+      ),
+    );
+  }
+
+  @override
+  void onTrayMenuItemClick(MenuItem menuItem) {
+    switch(menuItem.key) {
+      case 'show':
+        debugPrint("show");
+        windowManager.show(inactive: true).then((value) => null);
+        trayManager.destroy().then((value) => null);
+        break;
+      case 'quit':
+        trayManager.destroy().then((value) => exit(0));
+    }
+  }
+
+  @override
+  void onTrayIconRightMouseDown() {
+    trayManager.popUpContextMenu();
+  }
+}
